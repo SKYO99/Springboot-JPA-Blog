4be07ed3e437e91b110d7f8a892ec335540cfac3
YeonOh Choi <cjh10332@naver.com>
2023-09-08 16:39:07 +0900

    환경세팅 완료

---
+++ Hello/src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+	//스프링부트 연습을 위한 첫번째 테스트
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+
+	}
+}
